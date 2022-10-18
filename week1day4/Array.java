60e8b34fbd55ed8bddccc4f602613e4ae1e0a61b
AzhaguManickam <115480212+AzhaguManickam@users.noreply.github.com>
2022-10-18 23:30:27 +0530

    Add files via upload

---
+++ week1day4/Array.java
@@ -0,0 +1,12 @@
+package week1day4;
+
+public class Array {
+	public static void main(String[] args) {
+		String[] array= {"tvs","am","avm","nm"};
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+			
+		}
+	}
+
+}
