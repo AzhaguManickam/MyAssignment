509fb4d0f69fa821b9a0010aec9c72cee619a2f6
AzhaguManickam <115480212+AzhaguManickam@users.noreply.github.com>
2022-10-18 23:27:28 +0530

    Add files via upload

---
+++ day2/EmployeeDetails2.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class EmployeeDetails2 {
+	public static void main(String[] args) {
+		empDetails Details=new empDetails();
+		Details.empName();
+		Details.empId();
+		Details.empSalary();
+	}
+
+}
