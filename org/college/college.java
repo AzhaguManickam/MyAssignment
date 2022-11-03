8bd5675b0e84b461e8935735ac5f26648e7afed9
AzhaguManickam <115480212+AzhaguManickam@users.noreply.github.com>
2022-11-03 11:05:38 +0530

    Add files via upload

---
+++ org/college/college.java
@@ -0,0 +1,15 @@
+package org.college;
+
+public class college {
+	public void collegeName(String clgName) {
+		System.out.println("collegeName"+": "+clgName);
+	}
+	public void collegeCode(String clgCode){
+		System.out.println("CollegeCode"+": "+clgCode);
+	}
+	public void CollegeRank(int clgRank) {
+		System.out.println("collegeRank"+": "+clgRank);
+	}
+		
+
+}
---
+++ org/department/Department.java
@@ -0,0 +1,10 @@
+package org.department;
+
+import org.college.college;
+
+public class Department extends college {
+	public void depName(String depName) {
+		System.out.println("DepartmentName"+": "+depName);
+	}
+
+}
